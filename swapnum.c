c4ad2c470c0a0dadcabfef801d36148310317827
Amit Kushwaha <145186713+Amit72777@users.noreply.github.com>
2023-11-09 22:38:56 +0530

    using function
    using function we swap the two number

---
+++ swapnum.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    int a=10,b=20;
+     
+     swap(a,b);
+     printf("\n a=%d \t b=%d ",a,b);
+    return 0;
+}
+swap(  m,  n)
+ int m;
+int n;
+{
+    int t;
+    t=m;
+    m=n;
+    n=t;
+    printf(" m=%d n=%d ",m,n);
+    return m,n;
+}
