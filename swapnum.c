142200fb0d7db3a5b8120dbb9b7ede5cc4491d8a
Amit Kushwaha <amitkushw2005@gmail.com>
2024-01-04 02:17:33 +0530

    update the code

--- swapnum.c
+++ swapnum.c
@@ -2,20 +2,20 @@
 #include <math.h>
 int main()
 {
-    int a=10,b=20;
-     
-     swap(a,b);
-     printf("\n a=%d \t b=%d ",a,b);
+    int a = 10, b = 20;
+
+    swap(a, b);
+    printf("\n a=%d \t b=%d ", a, b);
     return 0;
 }
-swap(  m,  n)
- int m;
+int swap(m, n)
+int m;
 int n;
 {
     int t;
-    t=m;
-    m=n;
-    n=t;
-    printf(" m=%d n=%d ",m,n);
-    return m,n;
+    t = m;
+    m = n;
+    n = t;
+    printf(" m=%d n=%d ", m, n);
+    return m, n;
 }
--- towerofhanoi.c
+++ towerofhanoi.c
@@ -1,26 +1,26 @@
-// this programm is hack the path of Tower of Hanoi pazzel game  
- 
- #include<stdio.h>
-void tower(int n,char s,char h,char d)
+// this programm is hack the path of Tower of Hanoi pazzel game
+
+#include <stdio.h>
+void tower(int n, char s, char h, char d)
 {
-    if(n==0) return ;
-     tower ( n-1,s,d,h);
+    if (n == 0)
+        return;
+    tower(n - 1, s, d, h);  
 
-     printf("%c --> %c \n",s,d);
+    printf("%c --> %c \n", s, d);  // print the step 
 
-     tower( n-1, h,s,d);
-     return ;
-
+    tower(n - 1, h, s, d);  // function call itself 
+    return;
 }
 
 int main()
 {
     int n;
-    
-    printf("Enter the number :");
-    scanf("%d",&n);
 
-    tower( n,'A','B','C');
+    printf("Enter the number :");
+    scanf("%d", &n);
+
+    tower(n, 'A', 'B', 'C');
 
     return 0;
 }
